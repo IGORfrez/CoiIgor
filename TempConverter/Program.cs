cee3d62d103b443797d5845631d6b74826fb1b26
Игорь Цой <cojigor55@gmail.com>
2026-09-13 15:17:19 +0300

    Lab01 task 2: temperature converter

---
+++ TempConverter/Program.cs
@@ -0,0 +1,5 @@
+﻿const double kelvinOffset = 273.15;
+double celsius = 23.5;
+double fahrenheit = celsius * 9 / 5 + 32;
+double kelvin = celsius + kelvinOffset;
+Console.WriteLine($"{celsius} °C = {fahrenheit} °F = {kelvin} K");
